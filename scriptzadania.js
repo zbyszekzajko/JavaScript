2fe558001bf6e07aadc3adace305ab8a3c562e1f
zbyszekzajko <zbyszekzajko@gmail.com>
2023-04-02 20:06:13 +0200

    task 18,19,20,21

--- scriptzadania.js
+++ scriptzadania.js
@@ -203,8 +203,8 @@ const camelCase = () => {
 let runNumber = Math.floor(Math.random() * 10) + 1;
 const game = () => {
 	let guessNumber = parseInt(document.querySelector("#guessNumber").value);
-console.log(runNumber);
-console.log(guessNumber);
+	console.log(runNumber);
+	console.log(guessNumber);
 	if (runNumber == guessNumber)
 		console.log(`Brawo zgadłeś!!! Wylosowana liczba to ${guessNumber}`);
 	else if (runNumber < guessNumber)
@@ -219,3 +219,104 @@ console.log(guessNumber);
 // literą ani cyfrą (np. !, @, # itp.). Jeśli hasło spełnia powyższe warunki, program
 // powinien wyświetlić komunikat "Hasło jest bezpieczne". W przeciwnym razie program
 // powinien wyświetlić komunikat "Hasło nie spełnia wymagań bezpieczeństwa".
+
+// 16. Napisz program, który pobiera od użytkownika dwie liczby całkowite i
+// wyświetla wszystkie liczby między nimi, włącznie z tymi liczbami. Na
+// przykład, jeśli użytkownik wprowadzi liczby 3 i 8, program powinien
+// wyświetlić ciąg: 3, 4, 5, 6, 7, 8.
+// a. Posłuż się pętlą for
+
+const twoNumber = () => {
+	const tab16 = [];
+	let number1 = document.getElementById("number1").value;
+	let number2 = document.getElementById("number2").value;
+
+	for (let i = number1; i <= number2; i++) {
+		tab16.push(i);
+	}
+	document.getElementById("16").innerHTML = `${tab16.join(" ")}`;
+};
+
+// 17. Napisz program, który prosi użytkownika o wprowadzenie listy liczb,
+// oddzielonych przecinkami. Program powinien następnie obliczyć
+// sumę tych liczb i wyświetlić ją na ekranie.
+// a. Skorzystaj z funkcji split()
+
+const task17 = () => {
+	sum = 0;
+	let number17 = document.getElementById("number17").value;
+	tab17 = number17.split(",");
+	tab17.forEach(element => {
+		sum += Number(element);
+	});
+
+	document.getElementById("17").innerHTML = `${sum}`;
+};
+
+// 18. Napisz program, który wypełni tablicę losowymi liczbami całkowitymi
+// z przedziału od 1 do 100, o rozmiarze podanym przez użytkownika.
+// Następnie program powinien znaleźć i wyświetlić największą liczbę w
+// tej tablicy.
+
+const randomNumber18 = () => {
+	const arr18 = [];
+	let n = Number(document.getElementById("number18").value);
+	for (let i = 1; i <= n; i++) {
+		arr18.push(Math.floor(Math.random() * 100) + 1);
+	}
+	console.log(arr18);
+	document.getElementById("18").innerHTML = `Najwieksza liczba jest ${Math.max(
+		...arr18
+	)}`;
+};
+
+// 19. Napisz program, który losuje liczby z zakresu od 1 do 50, dopóki ich
+// suma nie przekroczy 200. Następnie program powinien usunąć
+// wszystkie liczby parzyste z wylosowanej tablicy i wyświetlić na
+// ekranie tablicę z pozostałymi liczbami.
+// a. Podpowiedź: zamiast usuwania, łatwiejsze może być
+// przepisanie nieparzystych wartości do nowej tablicy.
+
+const randomNumber19 = () => {
+	let sum = 0;
+	let arr19 = [];
+	let arr19Odd = [];
+	let num = 0;
+	while (sum <= 200) {
+		num = Number(Math.floor(Math.random() * 50) + 1);
+		sum += num;
+		if (sum <= 200) {
+			arr19.push(num);
+			if (num % 2 !== 0) arr19Odd.push(num);
+		}
+	}
+	console.log(sum);
+	console.log(arr19);
+	console.log(arr19Odd);
+};
+
+randomNumber19();
+
+// 20. Napisz program, który pobiera od użytkownika listę nazwisk
+// oddzielonych przecinkami. Program powinien następnie posortować
+// te nazwiska według długości nazwiska (od najkrótszego do
+// najdłuższego) i wyświetlić posortowaną listę na ekranie.
+// a. Posłuż się funkcją sort(), podając jako parametr własną funkcję
+// porównującą
+
+const surnameSort = () => {
+	surname20 = document.getElementById("surname20").value;
+	let arr20 = surname20.split(",");
+	console.log(arr20);
+	arr20.sort((a, b) => a.length - b.length);
+	console.log(arr20);
+	document.getElementById("20").innerHTML = `${arr20.join(", ")}`;
+};
+// 6. Napisz program, który poprosi użytkownika o wprowadzenie listy
+// towarów oddzielonych przecinkami. Następnie program powinien
+// utworzyć tablicę obiektów, gdzie każdy obiekt zawiera nazwę towaru
+// i losową cenę. Cena powinna być wylosowana z przedziału od 0.01
+// do 99.99. Wyświetl zawartość tablicy.
+// 7. Zadeklaruj (w kodzie programu) tablicę studentów, gdzie każdy
+// student ma imię, nazwisko i liczbę punktów (od 0 do 100). Następnie
+// oblicz średnią punktów i wypisz dane studentów którzy zdobyli
